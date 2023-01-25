a83459bbb0af852604291f7aa1fce860407a8414
Matteogatto94 <cosenza94@live.it>
2023-01-25 16:23:33 +0100

    create a view and add methods to see mail

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,21 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <h1>Home Page</h1>
-    </div>
+    <nav id="navbar" class="nav-menu navbar">
+        <ul>
+            <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+            <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+            <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a>
+            </li>
+        </ul>
+    </nav>
+
+    <section id="hero" class="d-flex flex-column justify-content-center align-items-center jumbotron_home">
+        <div class="hero-container">
+            <h1>Matteo Gatto</h1>
+            <p>I'm Full Stack Web Developer Student</p>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
